accae4a76fd2f031ef4f93a47988b0e92a78f897
Frostzeichen <cruztjomar@gmail.com>
2025-04-28 18:33:17 +0800

    second commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,57 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Library for disabling brownout connector
+// #include "soc/soc.h"
+// #include "soc/rtc_cntl_reg.h"
+
+char wSsid[] = "GlobeAtHome_F3398";
+char wPassword[] = "AC1656F1";
+
+int led = 2;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+
+  WiFi.begin(wSsid, wPassword);
+
+  Serial.print("Connecting to WiFi router");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(250);
+    Serial.print(".");
+  }
+  // server.begin();
+  Serial.println();
+
+  Serial.print("Connected! @ address: ");
+  Serial.println(WiFi.localIP());
+
+  pinMode(led, OUTPUT);
+
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    WiFiClient client;
+
+    http.begin(client, "http://192.168.254.125:3000/"); // Replace with your URL
+    int httpResponseCode = http.GET(); // Send the request
+
+    if (httpResponseCode > 0) {
+      String payload = http.getString(); // Get the response payload
+      Serial.println(httpResponseCode); // Print HTTP response code
+      Serial.println(payload); // Print the response payload
+    } else {
+      Serial.print("Error on HTTP request: ");
+      Serial.println(httpResponseCode);
+    }
+    
+    http.end(); // Free resources
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(led, HIGH);
+  delay(100);
+  digitalWrite(led, LOW);
+  delay(100);
 }

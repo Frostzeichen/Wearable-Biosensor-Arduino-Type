ff0ab3a509d47a524b22851bf2f80edd00a676f9
Frostzeichen <cruztjomar@gmail.com>
2025-05-17 01:28:44 +0800

    feat: add bearer token and edit url

--- include/env_example.h
+++ include/env_example.h
@@ -1,8 +1,10 @@
 #ifndef ENV_H
 #define ENV_H
 
-char wSsid[] = "example ssid";
-char wPassword[] = "example passoword";
-char apiUrlEsrand[] = "https://esrand-embedded-systems-rest-api-ne-git-d922e1-terenzs-projects.vercel.app/";
+char wSsid[] = "your wifi SSID here";
+char wPassword[] = "your wifi password here";
+// char apiUrlEsrand[] = "https://esrand-embedded-systems-rest-api-ne-git-d922e1-terenzs-projects.vercel.app/";
+char apiUrlEsrand[] = "https://hydration-backend-production.up.railway.app/data/receive";
+char bearerToken[] = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyMSIsImV4cCI6MTc0NzQxNjc2Mn0.aNwENzpYPhJK_D7v9CfWRqpkyzLMd9fiUJFZBIDBHPU";
 
 #endif
